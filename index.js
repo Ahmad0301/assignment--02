0a5eed2f3e727e0af64c60bb0dc4db1a8a643e08
Ahmad0301 <sehroz3040@gmail.com>
2024-11-30 15:30:47 +0500

    creat post api

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ const db = require('./db')
 const books = require('./models/books')
 const members = require ('./models/member')
 const Transaction = require('./models/transaction')
+
+app.post ('/creat', async(req,resp)=>{
+    const {book,member,transaction} = req.body
+    const bookobj = await books.create(book)
+    const memberobj= await members.create(member)
+    transaction.bookId = bookobj._id
+    transaction.memberId=memberobj._id
+    const transactionobj = await Transaction.create(transaction)
+    resp.json({"Book": bookobj, "member":memberobj,"transaction": transactionobj})
+})
+

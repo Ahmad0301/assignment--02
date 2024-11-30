5d67ead6291e97705b4479333a29686fd6025157
Ahmad0301 <sehroz3040@gmail.com>
2024-11-30 15:29:31 +0500

    creat a datbase connection  and export it

---
+++ db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/')
+const db = mongoose.connection
+db.on ('connected',()=>{console.log('mongodb is connected')})
+db.on ('erroe',()=>{console.log('error in connection')})
+db.on ('disconnected',()=>[consol.log('mongodb is disconnected')])
+
+module.exports = db

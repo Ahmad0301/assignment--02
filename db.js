02ca09e6d635b61489698feab0dce55b0b6e7ab3
Ahmad0301 <sehroz3040@gmail.com>
2025-01-05 20:05:39 +0500

    correct error

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/')
+mongoose.connect('mongodb://localhost:27017/assignment_02')
 const db = mongoose.connection
 db.on ('connected',()=>{console.log('mongodb is connected')})
 db.on ('erroe',()=>{console.log('error in connection')})
